d99eedd69599e18d9d9ab93498968e118fbdf546
Keigo Oka <oka@google.com>
2023-09-25 09:20:52 +0000

    ide: tast: Extract out a common test set up
    This change extracts out a function that would be commonly used for
    unit-testing Tast features, for common set up. A succeeding change
    will demonstrate its usage from another unit test.
    
    BUG=b:301891679
    TEST=npm run preupload
    
    Change-Id: I8922d398f2229330a6e99aca5ef0eaab131b9af4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/4886084
    Tested-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Eric Lok <lokeric@google.com>
    Commit-Queue: Keigo Oka <oka@chromium.org>

--- cros-ide/src/test/unit/features/chromiumos/device_management/commands/tast/run_tast_tests.test.ts
+++ cros-ide/src/test/unit/features/chromiumos/device_management/commands/tast/run_tast_tests.test.ts
@@ -2,25 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import * as path from 'path';
 import * as vscode from 'vscode';
 import * as commonUtil from '../../../../../../../common/common_util';
-import {CommandContext} from '../../../../../../../features/device_management/commands/common';
 import {
   RunTastTestsResult,
   runTastTests,
 } from '../../../../../../../features/device_management/commands/tast/run_tast_tests';
-import {DeviceCategory} from '../../../../../../../features/device_management/device_repository';
-import {Metrics} from '../../../../../../../features/metrics/metrics';
 import {ChrootService} from '../../../../../../../services/chromiumos';
 import * as testing from '../../../../../../testing';
-import {
-  FakeTextDocument,
-  VoidOutputChannel,
-  installChrootCommandHandler,
-} from '../../../../../../testing/fakes';
-import {FakeDeviceRepository} from '../../fake_device_repository';
-import {FakeSshServer} from '../../fake_ssh_server';
+import {installChrootCommandHandler} from '../../../../../../testing/fakes';
+import {arrayWithPrefix} from '../../../../../testing/jasmine/asymmetric_matcher';
+import {prepareCommonFakes} from './common';
 
 describe('runTastTests', () => {
   const {vscodeSpy, vscodeEmitters, vscodeGetters} =
@@ -31,79 +23,50 @@ describe('runTastTests', () => {
 
   const tempDir = testing.tempDir();
 
+  const subscriptions: vscode.Disposable[] = [];
+  afterEach(() => {
+    vscode.Disposable.from(...subscriptions.splice(0).reverse()).dispose();
+  });
+
   it('runs Tast tests on device', async () => {
     // Prepare a fake chroot.
     const chromiumos = tempDir.path as commonUtil.Source;
-    await testing.buildFakeChroot(chromiumos);
 
-    // Prepare a fake device.
-    const sshServer = new FakeSshServer();
-    await sshServer.listen();
-    const port = sshServer.listenPort;
-    const hostname = `localhost:${port}`;
-
-    fakeExec
-      .withArgs('ssh', jasmine.anything(), jasmine.anything())
-      .and.callThrough();
-
-    const deviceRepository = FakeDeviceRepository.create([
-      {hostname: hostname, category: DeviceCategory.OWNED},
-      {hostname: 'other', category: DeviceCategory.OWNED},
-    ]);
-
-    // Prepare a fake Tast test.
-    vscodeGetters.window.activeTextEditor.and.returnValue({
-      document: new FakeTextDocument({
-        uri: vscode.Uri.file(
-          path.join(
-            chromiumos,
-            'src/platform/tast-tests/src/go.chromium.org/tast-tests/cros/local/bundles/cros/example/chrome_fixture.go'
-          )
-        ),
-        text: `func init() {
+    const context = await prepareCommonFakes(
+      fakeExec,
+      vscodeGetters,
+      vscodeSpy,
+      {
+        chromiumos,
+        activeTextEditor: {
+          path: 'src/platform/tast-tests/src/go.chromium.org/tast-tests/cros/local/bundles/cros/example/chrome_fixture.go',
+          text: `func init() {
   testing.AddTest(&testing.Test{
     Func: ChromeFixture,
   })
 }
 
-func ChromeFixture(ctx context.Context, s *testing.State) {}`,
-      }) as vscode.TextDocument,
-    } as vscode.TextEditor);
-
-    // Prepare user responses.
-    spyOn(Metrics, 'send');
-    vscodeSpy.window.showQuickPick
-      .withArgs([hostname, 'other'], jasmine.anything())
-      .and.resolveTo(hostname);
-    vscodeSpy.window.showQuickPick
-      .withArgs(['example.ChromeFixture'], jasmine.anything())
-      .and.resolveTo('example.ChromeFixture');
+func ChromeFixture(ctx context.Context, s *testing.State) {}
+`,
+        },
+        tastListResult: 'example.ChromeFixture\n',
+        testToPick: 'example.ChromeFixture',
+      },
+      subscriptions
+    );
 
     // Prepare external command responses.
     installChrootCommandHandler(
       fakeExec,
       chromiumos,
       'tast',
-      jasmine.anything(),
-      async args => {
-        switch (args[0]) {
-          case 'list':
-            return 'example.ChromeFixture\n';
-          case 'run':
-            return ''; // OK
-        }
-        return new Error(`unsupported: tast ${args}`);
-      }
+      arrayWithPrefix('run'),
+      async () => '' // OK
     );
 
     // Test.
     const result = await runTastTests(
-      {
-        deviceRepository,
-        sshIdentity: sshServer.sshIdentity,
-        sshSessions: new Map(),
-        output: new VoidOutputChannel() as vscode.OutputChannel,
-      } as CommandContext,
+      context,
       ChrootService.maybeCreate(chromiumos, /* setContext = */ false)!
     );
 
