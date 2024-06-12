9100f3b971d4a5bcf51e9208e9794fca33aa55a1
Keigo Oka <oka@google.com>
2024-06-12 18:19:09 +0000

    Add a command to run unit tests in paralell as an experiment
    root_cause_flakiness.ts should be updated as a follow up to support the
    parallel mode so we can diagnose flakiness if happens.
    
    BUG=b:346849057
    TEST=time npm run unit-test          # 42.314 total
    TEST=time npm run unit-test-parallel # 11.625 total
    
    Change-Id: I8f0711dd4e291396a22c248d8d86be2e00684737
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/infra/ide/+/5628232
    Reviewed-by: Grace Cham <hscham@chromium.org>
    Tested-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>

---
+++ chromiumide/tools/run_unit_tests_in_parallel.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Copyright 2024 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Runs unit tests in parallel by running jasmine with the --parallel flag. The script kills the
+# worker processes Jasmine spawns when it is run with the --parallel flag, because otherwise the
+# wireit command doesn't exit, waiting for the worker processes to finish.
+
+# Usage: ./tools/run_unit_tests_in_parallel.sh
+
+cd "$(dirname "$0")/.." || exit $?
+
+# Kills all the processes with the given process group ID.
+kill_all() {
+  pgid="$1"
+  pkill -g "${pgid}"
+}
+
+# Outputs the process group of the given process.
+get_pgid() {
+  # xargs to trim spaces
+  ps -o pgid= -p "$1" | xargs || exit 1
+}
+
+# Internal usage only. It runs the actual unit tests, outputting the process group to the given file
+# to make the process group management possible.
+if [[ -n "$1" ]]; then
+  pgid="$(get_pgid $$)"
+
+  echo "${pgid}" > "$1"
+
+  env NODE_OPTIONS='-r source-map-support/register' NODE_PATH=out/src/test/unit/injected_modules npx jasmine --config=src/test/unit/jasmine.json --parallel=auto --color
+
+  status=$?
+
+  kill_all "${pgid}"
+
+  exit "${status}"
+fi
+
+pgid_file="$(mktemp)"
+trap 'rm ${pgid_file}' EXIT
+
+setsid ./tools/run_unit_tests_in_parallel.sh "${pgid_file}" &
+
+my_pid=$$
+
+trap 'kill_all $(cat ${pgid_file}) && kill ${my_pid}' INT TERM
+
+wait
