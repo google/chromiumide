5f60913a6797747b4ff4a69b720099e338dabc7e
Keigo Oka <oka@google.com>
2025-05-08 04:33:45 +0000

    java: adapt to the change recently made in the script we use
    https://crrev.com/c/6495888 changed the format of the json files the
    list_java_targets.py generates. This change adapt to the change so that
    the values needed for building java code are properly retrieved.
    
    Bug: 413331591, 416015041
    Test: npm t, manually open a java file and confirm build succeeds
    Change-Id: I1af6008ab4eda666a349a47a226e2b096176fbb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/infra/ide/+/6521547
    Tested-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>

--- chromiumide/src/test/testdata/java/src/build/android/list_java_targets.py
+++ chromiumide/src/test/testdata/java/src/build/android/list_java_targets.py
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "//chrome:chrome: ${PWD}/out/current_link/gen/chrome/chrome_java.build_config.json"
-echo "//content:content: ${PWD}/out/current_link/gen/content/content_java.build_config.json"
-echo "//third_party/android_sdk:android_sdk: ${PWD}/out/current_link/gen/third_party/android_sdk/android_sdk_java.build_config.json"
+echo "//chrome:chrome: ${PWD}/out/current_link/gen/chrome/chrome_java.params.json"
+echo "//content:content: ${PWD}/out/current_link/gen/content/content_java.params.json"
+echo "//third_party/android_sdk:android_sdk: ${PWD}/out/current_link/gen/third_party/android_sdk/android_sdk_java.params.json"
